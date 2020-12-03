6ab1be595ca3fc1d572c5fcdb36970faec802cb4
duong2020e <72533430+duong2020e@users.noreply.github.com>
2020-12-02 21:02:58 -0700

    Update khoiluong.c

--- Bài tập 2/khoiluong.c
+++ Bài tập 2/khoiluong.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
+  // khai bao bien m
   int m;
+  // xuat m ra man hinh
+  printf("Nhap m = ");
   scanf("%d", &m);
-  printf("kg = %dg\n", m * 1000);
-  printf("KG  | G\n");
+  // xuat kg = g ra man hinh
+  printf("kg = %dg\n--------\n", m * 1000);
+  //xuat kg,g thanh hai cot
+  printf("KG   | G\n--------\n");
   printf("%d", m);
   printf("   |%d", m * 1000);
   return 0;
