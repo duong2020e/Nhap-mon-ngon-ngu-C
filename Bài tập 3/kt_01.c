25a50e723d8a22498c4aef634e755aa27b61924b
duong2020e <72533430+duong2020e@users.noreply.github.com>
2021-01-06 21:05:22 -0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main(){
-  int i,j;
-    for(i = 1; i <= 9;i++){
-        printf("\n ---BANG NHAN--- %d\n", i);
-        for(j = 0; j <= 10; j++){
-            printf("%d x %d = %d\n", i, j, i*j);
-        }
+  int a,b,c;
+  a = 2; b = 3; c = 4;
+  printf("bang 2\n");
+  for(int e = 1; e <=10; e++){
+      printf("%d x %d = %d\n",a,e,a * e);
     }
+    printf("bang 3\n");
+  for(int f = 1; f <=10; f++){
+      printf("%d x %d = %d\n",b,f,b * f);
+    }
+    printf("bang 4\n");
+  for(int g = 1; g <=10; g++){
+    
+      printf("%d x %d = %d\n",c,g,c * g);
+    }
+return 0;
 }
