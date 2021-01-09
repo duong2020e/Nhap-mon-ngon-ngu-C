4f67b1ba13a92d98309d0b7efc7d9994ce0e1f02
duong2020e <72533430+duong2020e@users.noreply.github.com>
2021-01-08 20:25:58 -0700

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int h;
+  //h = 4: 4 hàng
+  //hàng 1:1 sao 6 khoảng trống
+  //hàng 2:3 sao 6 khoảng trống
+  //hàng 3:5 sao 6 khoảng trống
+  //hàng 4:7 sao 6 khoảng trống
+  //h = 3: 3 hàng
+  //hàng 1:1 sao 6 khoảng trống
+  //hàng 2:3 sao 6 khoảng trống
+  //hàng 3:5 sao 6 khoảng trống
+  //h = 2: 2 hàng
+  //hàng 1:1 sao 6 khoảng trống
+  //hàng 2:3 sao 6 khoảng trống
+  //h = 1: 1 hàng
+  //hàng 1:1 sao 6 khoảng trống
+  return 0;
+}
